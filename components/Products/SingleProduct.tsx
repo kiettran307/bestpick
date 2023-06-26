9700853d115c3e62fd04d01be967b5f7e813a237
Vo Anh Tuan <tuanvo19991604@gmail.com>
2023-06-25 20:26:34 -0700

    Update Ui

--- components/Products/SingleProduct.tsx
+++ components/Products/SingleProduct.tsx
@@ -3,15 +3,20 @@ import Image from "next/image";
 
 export function SingleProduct({ product }: { product: Product }): JSX.Element {
   return (
-    <div className="flex w-[384px] flex-col justify-center">
+    <div className="flex w-full max-w-[384px] flex-col justify-center">
       <div>
-        <Image src={product.image} alt="aaa" width={384} height={267} />
+        <Image
+          src={product.image}
+          alt="aaa"
+          style={{ width: "100%", height: "auto" }}
+          width={0}
+          height={0}
+          sizes="100vw"
+        />
       </div>
       <div className="flex flex-col pt-6">
-        <div className="flex w-[352px] px-4 text-2xl text-black">
-          {product.title}
-        </div>
-        <div className="flex w-[352px] px-4 text-lg text-black">
+        <div className="flex  px-4 text-2xl text-black">{product.title}</div>
+        <div className="flex w-full px-4 text-lg text-black">
           {product.description}
         </div>
         <div className="mt-3.5 flex px-4 text-4xl font-bold">
